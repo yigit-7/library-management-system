c64528c6998ca858fb466714be4ba0569b6f9cfd
yigit-7 <yigit.girit@hotmail.com>
2025-12-16 16:55:40 +0300

    feat: add query methods to Author and Category repositories
    Added custom methods for case-insensitive name searches and pagination support in both Author and Category repositories. Also included an existence check and an optional retrieval method for Category by name.

--- apps/spring-boot-app/src/main/java/me/seyrek/library_management_system/author/repository/AuthorRepository.java
+++ apps/spring-boot-app/src/main/java/me/seyrek/library_management_system/author/repository/AuthorRepository.java
@@ -1,9 +1,12 @@
 package me.seyrek.library_management_system.author.repository;
 
 import me.seyrek.library_management_system.author.model.Author;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Page<Author> findByNameIsContainingIgnoreCase(String name, Pageable pageable);
 }
--- apps/spring-boot-app/src/main/java/me/seyrek/library_management_system/category/repository/CategoryRepository.java
+++ apps/spring-boot-app/src/main/java/me/seyrek/library_management_system/category/repository/CategoryRepository.java
@@ -1,9 +1,16 @@
 package me.seyrek.library_management_system.category.repository;
 
 import me.seyrek.library_management_system.category.model.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    boolean existsByName(String name);
+    Optional<Category> findByNameIgnoreCase(String name);
+    Page<Category> findByNameIsContainingIgnoreCase(String name, Pageable pageable);
 }
