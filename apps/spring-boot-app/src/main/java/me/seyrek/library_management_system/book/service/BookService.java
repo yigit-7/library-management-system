8dad7ad00122226357ad168da64f3338df06879a
yigit-7 <yigit.girit@hotmail.com>
2025-12-16 16:58:14 +0300

    feat: add dynamic filtering and validation for Book operations
    Updated `getAllBooks` to support dynamic query via `BookSearchRequest`. Introduced validation to prevent deletion of books with associated copies. Refactored methods for improved readability and consistency.

--- apps/spring-boot-app/src/main/java/me/seyrek/library_management_system/book/service/BookService.java
+++ apps/spring-boot-app/src/main/java/me/seyrek/library_management_system/book/service/BookService.java
@@ -3,12 +3,13 @@ package me.seyrek.library_management_system.book.service;
 import me.seyrek.library_management_system.book.dto.BookCreateRequest;
 import me.seyrek.library_management_system.book.dto.BookDto;
 import me.seyrek.library_management_system.book.dto.BookPatchRequest;
+import me.seyrek.library_management_system.book.dto.BookSearchRequest;
 import me.seyrek.library_management_system.book.dto.BookUpdateRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface BookService {
-    Page<BookDto> getAllBooks(Pageable pageable);
+    Page<BookDto> getAllBooks(BookSearchRequest request, Pageable pageable);
     BookDto getBookById(Long id);
     BookDto createBook(BookCreateRequest request);
     BookDto updateBook(Long id, BookUpdateRequest request);
