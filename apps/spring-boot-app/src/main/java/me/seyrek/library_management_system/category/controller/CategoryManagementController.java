35bc1a892793780ae5e31050c8bbbebf2bca4272
yigit-7 <yigit.girit@hotmail.com>
2025-12-16 17:00:33 +0300

    refactor: enhance functionality
    Improved `getCategories` method to support filtering by name. Added `@ResponseStatus` annotation for category creation to return proper HTTP status.

--- apps/spring-boot-app/src/main/java/me/seyrek/library_management_system/category/controller/CategoryManagementController.java
+++ apps/spring-boot-app/src/main/java/me/seyrek/library_management_system/category/controller/CategoryManagementController.java
@@ -1,26 +1,26 @@
 package me.seyrek.library_management_system.category.controller;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import me.seyrek.library_management_system.category.dto.CategoryCreateRequest;
 import me.seyrek.library_management_system.category.dto.CategoryDto;
 import me.seyrek.library_management_system.category.dto.CategoryUpdateRequest;
 import me.seyrek.library_management_system.category.service.CategoryService;
 import me.seyrek.library_management_system.common.ApiResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/management/categories")
+@RequestMapping("/api/management/categories")
 @PreAuthorize("hasRole('ADMIN')")
+@RequiredArgsConstructor
 public class CategoryManagementController {
 
     private final CategoryService categoryService;
 
-    public CategoryManagementController(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
-
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<CategoryDto> createCategory(@Valid @RequestBody CategoryCreateRequest request) {
         return ApiResponse.success(categoryService.createCategory(request));
     }
