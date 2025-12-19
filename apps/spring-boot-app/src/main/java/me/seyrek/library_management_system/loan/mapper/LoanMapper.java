23385de928da997e4141674ae5cb139de55d7dde
yigit-7 <yigit.girit@hotmail.com>
2025-12-19 23:41:09 +0300

    feat: add mappers for Loan and Fine entities
    Introduce LoanMapper and FineMapper with MapStruct to simplify DTO and entity conversions.

---
+++ apps/spring-boot-app/src/main/java/me/seyrek/library_management_system/loan/mapper/LoanMapper.java
@@ -0,0 +1,83 @@
+package me.seyrek.library_management_system.loan.mapper;
+
+import me.seyrek.library_management_system.fine.mapper.FineMapper;
+import me.seyrek.library_management_system.loan.dto.*;
+import me.seyrek.library_management_system.loan.model.Loan;
+import org.mapstruct.*;
+
+import java.time.Instant;
+
+@Mapper(componentModel = "spring", uses = {FineMapper.class})
+public interface LoanMapper {
+
+    // --- Main Mappers ---
+
+    @Mapping(source = "user.id", target = "userId")
+    @Mapping(source = "user.email", target = "userEmail")
+    @Mapping(source = "copy.id", target = "copyId")
+    @Mapping(source = "copy.book.title", target = "bookTitle")
+    @Mapping(source = "copy.book.isbn", target = "bookIsbn")
+    @Mapping(source = "copy.book.coverImageUrl", target = "bookCoverUrl")
+    @Mapping(source = "loan", target = "isOverdue", qualifiedByName = "isLoanOverdue")
+    LoanDto toLoanDto(Loan loan);
+
+    @Mapping(source = "user.id", target = "userId")
+    @Mapping(source = "user.email", target = "userEmail")
+    @Mapping(source = "user.firstName", target = "userFirstName") // Added
+    @Mapping(source = "user.lastName", target = "userLastName")   // Added
+    @Mapping(source = "copy.id", target = "copyId")
+    @Mapping(source = "copy.barcode", target = "copyBarcode")     // Added
+    @Mapping(source = "copy.book.title", target = "bookTitle")
+    @Mapping(source = "copy.book.isbn", target = "bookIsbn")
+    @Mapping(source = "copy.book.coverImageUrl", target = "bookCoverUrl")
+    @Mapping(source = "loan", target = "isOverdue", qualifiedByName = "isLoanOverdue")
+    LoanDetailDto toLoanDetailDto(Loan loan);
+
+    @Mapping(source = "copy.book.title", target = "bookTitle")
+    @Mapping(source = "copy.book.coverImageUrl", target = "bookCoverUrl")
+    @Mapping(source = "loan", target = "isOverdue", qualifiedByName = "isLoanOverdue")
+    LoanUserSummaryDto toLoanUserSummaryDto(Loan loan);
+
+
+    // --- Helper Methods ---
+
+    @Named("isLoanOverdue")
+    default boolean isLoanOverdue(Loan loan) {
+        if (loan.getReturnDate() != null) {
+            return loan.getReturnDate().isAfter(loan.getDueDate());
+        }
+        return Instant.now().isAfter(loan.getDueDate());
+    }
+
+    // --- Request Mappers ---
+
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "copy", ignore = true)
+    @Mapping(target = "loanDate", ignore = true)
+    @Mapping(target = "returnDate", ignore = true)
+    @Mapping(target = "fines", ignore = true)
+    Loan fromLoanCreateRequest(LoanCreateRequest request);
+
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "copy", ignore = true)
+    @Mapping(target = "loanDate", ignore = true)
+    @Mapping(target = "fines", ignore = true)
+    void updateLoanFromRequest(LoanUpdateRequest request, @MappingTarget Loan loan);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @InheritConfiguration(name = "updateLoanFromRequest")
+    void patchLoanFromRequest(LoanPatchRequest request, @MappingTarget Loan loan);
+
+    @Mapping(target = "userId", source = "userId")
+    @Mapping(target = "userEmail", ignore = true)
+    @Mapping(target = "copyId", ignore = true)
+    @Mapping(target = "barcode", ignore = true)
+    @Mapping(target = "bookId", ignore = true)
+    @Mapping(target = "loanDateStart", ignore = true)
+    @Mapping(target = "loanDateEnd", ignore = true)
+    @Mapping(target = "dueDateStart", ignore = true)
+    @Mapping(target = "dueDateEnd", ignore = true)
+    @Mapping(target = "returnDateStart", ignore = true)
+    @Mapping(target = "returnDateEnd", ignore = true)
+    LoanSearchRequest toLoanSearchRequest(LoanUserSearchRequest userRequest, Long userId);
+}
