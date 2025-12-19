fe9e45e5a8059d6a10263fc0945956f1f9ed072d
yigit-7 <yigit.girit@hotmail.com>
2025-12-19 23:57:21 +0300

    refactor: make Author to extend BaseEntity and add price

--- apps/spring-boot-app/src/main/java/me/seyrek/library_management_system/author/repository/AuthorRepository.java
+++ apps/spring-boot-app/src/main/java/me/seyrek/library_management_system/author/repository/AuthorRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+// TODO: really need to add Specification?
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
     Page<Author> findByNameIsContainingIgnoreCase(String name, Pageable pageable);
